d92182e757d6bc6c3bc391a31b9b12c1a5a63bca
Gaurav Rao <127169562+gaurav-rao1923@users.noreply.github.com>
2023-08-30 06:05:14 +0530

    Delete gcd.py

--- gcd.py
+++
@@ -1,9 +0,0 @@
-print("GCD of the given number is :")
-m=int(input("Please enter the value for m :"))
-n=int(input("please enter the value for n :"))
-while(n!=0):
-    r=m%n
-    m=n
-    n=r
-
-print(m)
