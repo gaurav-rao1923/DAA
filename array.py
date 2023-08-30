676e7fed3dff6b818abf0ff01e7692836246904a
Gaurav Rao <127169562+gaurav-rao1923@users.noreply.github.com>
2023-08-30 06:04:44 +0530

    Delete array.py

--- array.py
+++
@@ -1,8 +0,0 @@
-print("Create an array")
-array=[]
-for i in range(0,5):
-     array.append(input("Enter elements :"))
-b=int(input("Enter the index of the elements you wish to :"))
-for i in range (0,5):
-     if i==b:
-        print(array[i])
